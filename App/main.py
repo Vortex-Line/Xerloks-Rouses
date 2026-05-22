69730f35fd565f2f094baeb2c67f6af631d21ec1
anothergreb <raphaelgreb02012010@gmail.com>
2026-05-22 12:29:31 +0000

    Adding the .gitignore, and integrating the gemini API

--- App/main.py
+++ App/main.py
@@ -1,2 +1,14 @@
-text = "Test"
-print(text)
+import os
+from google import genai
+
+gemini_api_key = os.getenv('GEMINI_API_KEY')
+print(gemini_api_key)
+
+client = genai.Client(api_key = gemini_api_key)
+
+response = client.models.generate_content(
+    model="gemini-3.5-flash",
+    contents="Explain how AI works in a few words",
+)
+
+print(response.text)
