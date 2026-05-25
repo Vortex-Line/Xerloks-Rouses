761b34ba8cad7e1587285b233a75b891789ddd13
anothergreb <raphaelgreb02012010@gmail.com>
2026-05-25 15:44:41 +0000

    modified:   App/main.py 	new file:   App/website.py

--- App/main.py
+++ App/main.py
@@ -1,14 +1,18 @@
+from openrouter import OpenRouter
 import os
-from google import genai
 
-gemini_api_key = os.getenv('GEMINI_API_KEY')
-print(gemini_api_key)
+api_key = "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"
 
-client = genai.Client(api_key = gemini_api_key)
+user_input = input("Você: ")
 
-response = client.models.generate_content(
-    model="gemini-3.5-flash",
-    contents="Explain how AI works in a few words",
-)
-
-print(response.text)
+with OpenRouter(
+    api_key=api_key
+) as client:
+    response = client.chat.send(
+        model="openai/gpt-oss-120b:free",
+        messages=[
+            {"role": "user", "content": user_input + " responde de forma curta"}
+        ]
+    )
+    
+    print("Ia: ", response.choices[0].message.content)
