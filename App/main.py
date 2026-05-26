cbbcff4443c7ffe697d016f851ab866b93ef39f7
anothergreb <raphaelgreb02012010@gmail.com>
2026-05-26 11:25:05 +0000

    modified:   App/main.py

--- App/main.py
+++ App/main.py
@@ -3,16 +3,21 @@ import os
 
 api_key = "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"
 
-user_input = input("Você: ")
+def ia_chat():
+    while True:
+        user_input = input("Você: ")
 
-with OpenRouter(
-    api_key=api_key
-) as client:
-    response = client.chat.send(
-        model="openai/gpt-oss-120b:free",
-        messages=[
-            {"role": "user", "content": "Pegue está noticia e analise ela: " + user_input + " responda com Verdadeira ou Falsa."}
-        ]
-    )
-    
-    print("Ia: ", response.choices[0].message.content)
+        with OpenRouter(
+            api_key=api_key
+        ) as client:
+            response = client.chat.send(
+                model="openai/gpt-oss-120b:free",
+                messages=[
+                    {'role': 'system', 'content': 'Você é uma IA especialista em descobrir se uma informação que está na internet é verdadeira ou falsa, responda de forma curta.'},
+                    {"role": "user", "content": user_input}
+                ]
+            )
+            
+            print("Ia: ", response.choices[0].message.content)
+
+ia_chat()
