8bccc4a768f4461bc9d5dfda39ef76d9e6acd85a
anothergreb <raphaelgreb02012010@gmail.com>
2026-05-27 11:46:14 +0000

    modified:   App/UI.py

--- App/UI.py
+++ App/UI.py
@@ -1,15 +1,6 @@
 import streamlit as st
 
-st.title("Xerlock Rouses")
-st.markdown(
-    """ 
-    This is a playground for you to try Streamlit and have fun. 
-
-    **There's :rainbow[so much] you can build!**
-    
-    We prepared a few examples for you to get started. Just 
-    click on the buttons above and discover what you can do 
-    with Streamlit. 
-    """
-)
-
+st.title("Xerloks Rouses")
+st.chat_input()
+with st.chat_message("user"):
+    st.write("Hello! How can I help you today?")
